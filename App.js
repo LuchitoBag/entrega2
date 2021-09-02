4fc1e3ccf5f4417a0bd5612e9fe5c736d6217227
LuchitoBag <89401969+LuchitoBag@users.noreply.github.com>
2021-09-02 02:08:16 -0300

    entrega NavBar

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NabVar from './components/NavBar/NabVar';
+
+function App() {
+  return <NabVar />
+}
+
+export default App;
---
+++ components/NavBar/CartWidget.jsx
@@ -0,0 +1,12 @@
+
+
+const CartWidget = () => {
+    return (
+        <>
+          <a><img  className="w-25"  
+            src="https://w7.pngwing.com/pngs/225/984/png-transparent-computer-icons-shopping-cart-encapsulated-postscript-shopping-cart-angle-black-shopping.png"/></a>  
+        </>
+    )
+}
+
+export default CartWidget
